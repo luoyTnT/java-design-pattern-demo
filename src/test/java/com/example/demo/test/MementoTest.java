2565d3d325b46f93d53ad403fb9e210d00e34108
tanhaowen <luoy329@gmail.com>
2021-12-24 09:58:04 +0800

    feat: 备忘录模式

---
+++ src/test/java/com/example/demo/test/MementoTest.java
@@ -0,0 +1,22 @@
+package com.example.demo.test;
+
+import com.example.demo.memento.Caretaker;
+import com.example.demo.memento.Originator;
+import org.junit.jupiter.api.Test;
+
+public class MementoTest {
+
+    @Test
+    void mementoTest1() {
+        Originator or = new Originator();
+        Caretaker cr = new Caretaker();
+        or.setState("S0");
+        System.out.println("初始状态:" + or.getState());
+        cr.setMemento(or.createMemento()); //保存状态
+        or.setState("S1");
+        System.out.println("新的状态:" + or.getState());
+        or.restoreMemento(cr.getMemento()); //恢复状态
+        System.out.println("恢复状态:" + or.getState());
+    }
+
+}
